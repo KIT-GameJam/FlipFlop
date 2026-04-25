66a50de2d07c55e043f39c9e631f3ec877fcc33d
Dragadorice <ucshh@student.kit.edu>
2026-04-25 11:44:42 +0200

    Add portal animation

--- levels/portal/portal.gd
+++ levels/portal/portal.gd
@@ -3,7 +3,12 @@ extends Node2D
 
 @export var level := AbstractLevel.Level.Level1;
 @export_range(0, 10) var entrance := 0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		Global.game_manager.change_level(level, entrance)
+
+func _ready() -> void:
+	animation_player.play("spin")
