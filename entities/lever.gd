b1b525cddb888a86fe5a0d3db1b27d40bba7035d
Elias Schaut <eschaut@outlook.de>
2026-04-26 01:01:14 +0200

    move Lever to entities folder and add sprite integration

--- entities/lever.gd
+++ entities/lever.gd
@@ -1,7 +1,10 @@
 class_name Lever
 extends Area2D
 
+@export var sprite_black := false
 @export var toggle_blocks: Array[Vector2i]
+
+@onready var sprite: Sprite2D = $Sprite2D
 
 var toggled := false
 
@@ -14,6 +17,10 @@ const TILE_MAPPING: Dictionary[Array, Array] = {
 	[0, Vector2i(3, 3)]: [0, Vector2i(2, 3)],
 }
 
+func _ready() -> void:
+	if sprite_black:
+		modulate = Color.BLACK
+
 func toggle() -> void:
 	var tile_map: TileMapLayer = Global.game_manager.current_level_node.tile_map_layer
 	for block in toggle_blocks:
@@ -23,6 +30,7 @@ func toggle() -> void:
 		if mapping:
 			tile_map.set_cell(block, mapping[0], mapping[1])
 	toggled = not toggled
+	sprite.scale.x *= -1
 
 func reset() -> void:
 	if toggled:
