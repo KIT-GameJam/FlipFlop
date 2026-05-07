03cde44e81e900ff9f3a05037f360da5cc486be7
Elias Schaut <elias@schaut.dev>
2026-05-07 22:32:04 +0200

    add spawn_black property to enable black portal visuals and update related logic in transitions system

--- entities/portal/transitions.gd
+++ entities/portal/transitions.gd
@@ -1,3 +1,4 @@
+@tool
 class_name LevelTransition
 extends Area2D
 
@@ -7,10 +8,21 @@ enum Side { LEFT, RIGHT }
 
 @export var level := AbstractLevel.Level.Start
 @export var side := Side.RIGHT : set = _set_side
+@export var spawn_black := false
+
+@onready var door_frame: Line2D = $Visual/DoorFrame
+@onready var arrow_1: Polygon2D = $Visual/Arrow1
+@onready var arrow_2: Polygon2D = $Visual/Arrow2
+@onready var arrow_3: Polygon2D = $Visual/Arrow3
 
 @onready var visual: Node2D = $Visual
 
 func _ready() -> void:
+	if spawn_black:
+		door_frame.self_modulate = Color.BLACK
+		arrow_1.color = Color.BLACK
+		arrow_2.color = Color.BLACK
+		arrow_3.color = Color.BLACK
 	_apply_side()
 
 func _set_side(value: int) -> void:
